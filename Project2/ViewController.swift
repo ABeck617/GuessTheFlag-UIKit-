7fb39e304e1c36537a0226a2fcd828b279fc5c7e
Anthony Beckford <abeckford532@gmail.com>
2022-10-17 16:12:38 -0400

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Anthony Beckford on 10/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
