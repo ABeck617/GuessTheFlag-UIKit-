2794bf0f09c231c16c55f72e5423cbf2ec3c859c
Anthony Beckford <abeckford532@gmail.com>
2022-10-17 16:25:49 -0400

    Creating the logic for the GuessTheFlag Game

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var button1: UIButton!
-    
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
+    
+    // Creating new properties in a game
+    var countries = [String]()
+    var score = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // fill the countries array of strings with the flags in viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     }
 
-
+    
+// Created a new method called askQuestion
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
